a84cc8498b5b5ea67d2e765ef99d48b373a45f1d
Higor Alves <higorhaalves@gmail.com>
2022-05-25 10:29:12 -0700

    chore: base layout
    Signed-off-by: Higor Alves <higorhaalves@gmail.com>

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,6 +10,7 @@ import {
 	Menu,
 	useMantineTheme
 } from '@mantine/core'
+import Image from 'next/image'
 import {
 	HiFingerPrint,
 	HiOutlineBell,
@@ -25,47 +26,62 @@ export function Header() {
 	const { state } = useUser()
 	const [opened, setOpened] = useState<boolean>(false)
 	const theme = useMantineTheme()
+	const DARK_LOGO = '/assets/images/logo-dark.png'
+	const LIGHT_LOGO = '/assets/images/logo-light.png'
 
 	return (
 		<Container height={80} p='md' style={{ border: 0 }}>
-			<Group position={'right'} spacing={30} align={'center'}>
-				<Indicator inline color={'red'} style={{ marginTop: 5 }}>
-					<ActionIcon color={'gray'}>
-						<HiOutlineBell size={24} />
-					</ActionIcon>
-				</Indicator>
+			<Group grow>
+				<Group position={'left'}>
+					<Image
+						src={DARK_LOGO}
+						width={40}
+						height={40}
+						alt={'Novac Pro Logo'}
+					/>
+				</Group>
+				<Group position={'right'} spacing={30} align={'center'}>
+					<Indicator inline color={'red'} style={{ marginTop: 5 }}>
+						<ActionIcon color={'gray'}>
+							<HiOutlineBell size={24} />
+						</ActionIcon>
+					</Indicator>
 
-				<Menu
-					size={'lg'}
-					control={
-						<Avatar onClick={() => setOpened(!opened)} src={state?.photoURL} />
-					}
-				>
-					<Menu.Item
-						icon={<HiOutlineUser color={theme.colors.red[6]} size={18} />}
+					<Menu
+						size={'lg'}
+						control={
+							<Avatar
+								onClick={() => setOpened(!opened)}
+								src={state?.photoURL}
+							/>
+						}
 					>
-						Profile
-					</Menu.Item>
-					<Menu.Item
-						icon={<HiOutlineLibrary color={theme.colors.red[6]} size={18} />}
-					>
-						Banking Information
-					</Menu.Item>
+						<Menu.Item
+							icon={<HiOutlineUser color={theme.colors.red[6]} size={18} />}
+						>
+							Profile
+						</Menu.Item>
+						<Menu.Item
+							icon={<HiOutlineLibrary color={theme.colors.red[6]} size={18} />}
+						>
+							Banking Information
+						</Menu.Item>
+						<Divider />
+						<Menu.Item
+							icon={<HiFingerPrint color={theme.colors.red[6]} size={18} />}
+						>
+							Account settings
+						</Menu.Item>
+						<Menu.Item
+							icon={<HiOutlineLogout color={theme.colors.red[6]} size={18} />}
+							onClick={GoogleAuth.logout}
+							color={'red'}
+						>
+							Logout
+						</Menu.Item>
+					</Menu>
 					<Divider />
-					<Menu.Item
-						icon={<HiFingerPrint color={theme.colors.red[6]} size={18} />}
-					>
-						Account settings
-					</Menu.Item>
-					<Menu.Item
-						icon={<HiOutlineLogout color={theme.colors.red[6]} size={18} />}
-						onClick={GoogleAuth.logout}
-						color={'red'}
-					>
-						Logout
-					</Menu.Item>
-				</Menu>
-				<Divider />
+				</Group>
 			</Group>
 		</Container>
 	)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,19 @@ import {
 	Button,
 	Container,
 	Divider,
+	Grid,
 	Group,
+	List,
 	Space,
 	Text,
 	TextInput,
-	Title
+	ThemeIcon,
+	Title,
+	useMantineTheme
 } from '@mantine/core'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { HiCheck } from 'react-icons/hi'
 
 import { useUser } from '~/context'
 import { AUTH, GoogleAuth } from '~/services'
@@ -19,46 +25,83 @@ import { AUTH, GoogleAuth } from '~/services'
 export default function Home() {
 	const { dispatch } = useUser()
 	const router = useRouter()
+	const theme = useMantineTheme()
 
 	useEffect(() => {
 		onAuthStateChanged(AUTH, user => {
 			if (user) {
 				dispatch(user)
-				router.push('/dashboard')
 			}
 		})
 	}, [dispatch])
 
 	return (
-		<Container size={'xs'}>
-			<Title order={1}>Sign up</Title>
-			<Text weight={'bold'}>Sign up with Open account</Text>
-			<Space h={'xl'} />
-			<Group>
-				<Button
-					variant={'outline'}
-					color={'dark'}
-					size={'md'}
-					onClick={GoogleAuth.login}
+		<Grid align={'center'} style={{ minHeight: '100vh' }}>
+			<Grid.Col
+				span={3}
+				style={{
+					background: theme.colors.gray[1],
+					minHeight: '100vh',
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+					flexDirection: 'column'
+				}}
+			>
+				<Image
+					src={'/assets/images/login-pic.png'}
+					alt={'Mountains in a Square'}
+					width={180}
+					height={180}
+					objectFit={'contain'}
+					objectPosition={'center'}
+				/>
+				<Space h={'xl'} />
+				<Title order={1}>Novac.Pro - Finance</Title>
+				<Space h={'xl'} />
+				<List
+					icon={
+						<ThemeIcon color={'green'}>
+							<HiCheck size={20} />
+						</ThemeIcon>
+					}
 				>
-					Google
-				</Button>
-				<Button variant={'outline'} color={'dark'} size={'md'}>
-					Apple
-				</Button>
-			</Group>
+					<List.Item>
+						<Text weight={'bold'}>Invoice Tracker</Text>
+					</List.Item>
+				</List>
+			</Grid.Col>
+			<Grid.Col span={3} offset={3}>
+				<Container size={'xs'}>
+					<Title order={1}>Sign up</Title>
+					<Text weight={'bold'}>Sign up with social account</Text>
+					<Space h={'xl'} />
+					<Group>
+						<Button
+							variant={'outline'}
+							color={'dark'}
+							size={'md'}
+							onClick={GoogleAuth.login}
+						>
+							Google
+						</Button>
+					</Group>
 
-			<Space h={'xl'} />
-			<Divider />
-			<Space h={'xl'} />
+					<Space h={'xl'} />
+					<Divider />
+					<Space h={'xl'} />
 
-			<Text weight={'bold'}>Or continue with email address</Text>
-			<Space h={'md'} />
-			<TextInput placeholder={'Your email'} />
-			<Space h={'sm'} />
-			<Button color={'blue'} fullWidth>
-				Continue
-			</Button>
-		</Container>
+					<Text size={'sm'} color={'gray'} weight={'bold'}>
+						Or continue with email address
+					</Text>
+					<Space h={'md'} />
+					<TextInput placeholder={'Your email'} />
+					<Space h={'sm'} />
+					<Button color={'red'} fullWidth>
+						Continue
+					</Button>
+				</Container>
+			</Grid.Col>
+		</Grid>
 	)
 }
